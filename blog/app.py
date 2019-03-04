7609ba3088f96454a2396374ed4aec952d9dddc9
Sergiy Mushtyn <smushtyn@gmail.com>
2019-03-05 00:07:40 +0200

    Minor: Added menu() functionality
    RD: Added functionality to:
        - create the blog
        - list the blogs
        - read blog
        - create the post
        Unittests are added for functionality implemented

--- blog/app.py
+++ blog/app.py
@@ -1,3 +1,12 @@
+from blog import Blog
+POST_TEMPLATE = """
+--- {} ---
+
+{}
+
+"""
+
+MENU_PROMPT ='Enter "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create a post or "q" to quit: '
 blogs = dict()
 
 
@@ -7,9 +16,59 @@ def menu():
     #  Do smth with that choice
     #  Eventually exit
     print_blogs()
+    selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection == 'c':
+            ask_create_blog()
+        elif selection == 'l':
+            print_blogs()
+        elif selection == 'r':
+            ask_read_blog()
+        elif selection == 'p':
+            ask_create_post()
+        selection = input(MENU_PROMPT)
 
 
 def print_blogs():
     for _, blog in blogs.items():
         print("- {}".format(blog))
 
+
+def ask_create_blog():
+    title = input('Enter blog\'s title: ')
+    author = input('Enter your name: ')
+    blogs[title] = Blog(title, author)
+
+
+# own implementation of read blog
+# def ask_read_blog():
+#     blog_to_read = input('Enter the blog\'s title you want to read: ')
+#     try:
+#         blog = blogs[blog_to_read]
+#         print(blog.json())
+#     except KeyError:
+#         print('No such blog')
+
+def ask_read_blog():
+    blog_title = input('Enter the blog title you want to read: ')
+    print_posts(blogs[blog_title])
+
+
+def print_posts(blog):
+    for post in blog.posts:
+        print_post(post)
+
+
+def print_post(post):
+    print(POST_TEMPLATE.format(post.title, post.content))
+
+
+def ask_create_post():
+    blog_to_create_post_in = input('Enter the blog\'s name you want to add post in: ')
+    try:
+        blog = blogs[blog_to_create_post_in]
+        post_title = input('Enter the post\'s title: ')
+        post_content = input('Enter the post\'s content: ')
+        blog.create_post(post_title, post_content)
+    except KeyError:
+        print('No such blog')
--- blog/tests/system/app_test.py
+++ blog/tests/system/app_test.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from unittest.mock import patch
 import app
 from blog import Blog
+from post import Post
 
 
 class AppTest(TestCase):
@@ -10,9 +11,72 @@ class AppTest(TestCase):
     The 'print' func is patched and so that it is checking that the print func has been called with <some_value>
     """
 
+    def test_menu_calls_print_blogs(self):
+        """
+        Need to check whether 'print_blogs' func is called from the 'menu' func;
+        1. patch 'app.print_blogs' func to check whether it'll be called
+        2. As far as whole 'menu()' func will be called -> all instructions inside will be called including 'input'
+           which is waiting for the user's input, thus need to be handled to not stuck the test execution
+        3. Patch the 'builtins.input' func to replace 'input' with fake input func which will do nothing when
+           called -> thus will not cause test execution stucking.
+        """
+        with patch('app.print_blogs') as mocked_print_blogs:
+            with patch('builtins.input', return_value='q'):  # return_value assigns the value to a call
+                app.menu()
+                mocked_print_blogs.assert_called()
+
+    def test_mocked_input(self):
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+
     def test_print_blogs(self):
         blog = Blog("Test", "Sergiy Mushtyn")
         app.blogs = {'Test': blog}
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with("- Test by Sergiy Mushtyn: 0 posts")
+
+    def test_create_blog(self):
+        """
+        side_effect
+        If the patched 'input' func is called more than one time - the different args should be passed to it
+        patch(return_value=smth) -> will return single value on every patched func call
+        This is resolved by 'side_effect' -> the args are listed in that order in which they will be consequently passed
+        to the mocked func.
+        """
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('Test', 'Test Blog')
+
+            app.ask_create_blog()
+            self.assertIsNotNone(app.blogs.get('Test'))
+
+    def test_read_blog(self):
+        blog = Blog("Test", "Sergiy Mushtyn")
+        app.blogs = {'Test': blog}
+        with patch('builtins.input', return_value='Test'):
+            with patch('app.print_posts') as mocked_print_posts:
+                app.ask_read_blog()
+                mocked_print_posts.assert_called_with(blog)
+
+    def test_print_posts(self):
+        blog = Blog("Test", "Sergiy Mushtyn")
+        blog.create_post('Test Post', 'Test Content')
+        app.blogs = {'Test': blog}
+
+        with patch('app.print_post') as mocked_print_post:
+            app.print_posts(blog)
+            mocked_print_post.assert_called_with(blog.posts[0])
+
+
+    def test_print_post(self):
+        post = Post('Test Post', 'Test Content')
+        expected_post = """
+--- Test Post ---
+
+Test Content
+
+"""
+        with patch('builtins.print') as mocked_print:
+            app.print_post(post)
+            mocked_print.assert_called_with(expected_post)
