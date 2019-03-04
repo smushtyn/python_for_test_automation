97b314ff36ace701ff9ac6df2e63067db477589f
Sergiy Mushtyn <smushtyn@gmail.com>
2019-03-04 16:59:36 +0200

    Minor: Added AppTest
    RD: Added AppTest class with test_print_blogs() method
        that covers blog.<app.py>

--- blog/tests/system/app_test.py
+++ blog/tests/system/app_test.py
@@ -1,8 +1,18 @@
 from unittest import TestCase
 from unittest.mock import patch
 import app
+from blog import Blog
 
 
 class AppTest(TestCase):
+    """
+    To cover the function that printing smth out by the unit test the 'unittest.mock.patch' is used.
+    The 'print' func is patched and so that it is checking that the print func has been called with <some_value>
+    """
+
     def test_print_blogs(self):
-        pass
+        blog = Blog("Test", "Sergiy Mushtyn")
+        app.blogs = {'Test': blog}
+        with patch('builtins.print') as mocked_print:
+            app.print_blogs()
+            mocked_print.assert_called_with("- Test by Sergiy Mushtyn: 0 posts")
